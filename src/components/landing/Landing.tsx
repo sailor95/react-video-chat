aba5265dc6897747975d84d457bec52144b60b84
David Huang <bugman195@gmail.com>
2020-11-19 22:11:08 +0800

    Boilerplate Landing comp

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import styles from './styles.module.scss';
 
 const Landing = () => {
+  const [channel, setChannel] = useState('');
+  const [appId, setAppId] = useState('');
+  const [token, setToken] = useState('');
+  const [showHint, setShowHint] = useState(false);
+
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault();
+
+    if (!channel || !appId || !token) {
+      setShowHint(true);
+    }
+
+    console.log(`Channel: ${channel}, AppId: ${appId}, Token: ${token}`);
+    // TODO: Pass 3 values to Redux (dispatch action)
+    // TODO: Redirect to Room
+  };
+
   return (
     <div className={styles.container}>
-      <h1>Landing</h1>
+      <h1>Join a channel</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>App Id:</span>
+          <input
+            type="text"
+            value={appId}
+            onChange={e => setAppId(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Token:</span>
+          <input
+            type="text"
+            value={token}
+            onChange={e => setToken(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Channel:</span>
+          <input
+            type="text"
+            value={channel}
+            onChange={e => setChannel(e.target.value)}
+          />
+        </label>
+
+        {showHint && <p className={styles.hint}>Please enter all inputs</p>}
+
+        <button type="submit">Join Room</button>
+      </form>
     </div>
   );
 };
